507f2843b4ab57f1c514fe1bc843db7a1208f956
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2026-07-09 15:21:11 +0530

    Update run_seq.sh

--- Simulated Ensemble I/n,s effect with fixed B/run_seq.sh
+++ Simulated Ensemble I/n,s effect with fixed B/run_seq.sh
@@ -5,14 +5,14 @@ counter=0
 B=1000
 for n in $(seq 100 100 1000); do
 	echo $n
-	mkdir ProcessNets/$n
+	mkdir -p ProcessNets/$n
 	time python SIM_graphs_s.py $n $n $B >> "ProcessNets/$n/output.txt" 2>> "ProcessNets/$n/error.txt"
 	
 	for s in 75 100 150 200 300 500 600 700 1000 1500; do
 	{
 		echo $n $s
 		folder="$n/$s"
-		mkdir ProcessNets/graphs_$s ProcessNets/$folder ProcessNets/graphs_orig_$s
+		mkdir -p ProcessNets/graphs_$s ProcessNets/$folder ProcessNets/graphs_orig_$s
 		cp $s/[0-9]*.csv ProcessNets/graphs_$s
 		cp "$s/edge count.tsv" ProcessNets/$folder
 		cp ProcessNets/$n/seed.txt ProcessNets/$folder
