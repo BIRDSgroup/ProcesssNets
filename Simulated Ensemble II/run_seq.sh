e24dcf276b53c4b3d81489cf32332bc48881077b
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2026-07-09 15:22:53 +0530

    Update run_seq.sh

--- Simulated Ensemble II/run_seq.sh
+++ Simulated Ensemble II/run_seq.sh
@@ -5,11 +5,11 @@ counter=0
 B=1000
 for n in $(seq 100 100 1000); do
 	echo $n
-	mkdir ProcessNets/$n $n
+	mkdir -p ProcessNets/$n $n
 	time python SIM_graphs_p.py $n $n $B >> "ProcessNets/$n/output.txt" 2>> "ProcessNets/$n/error.txt"
 	echo $n
 	folder="$n"
-	mkdir ProcessNets/graphs_$n ProcessNets/graphs_orig_$n
+	mkdir -p ProcessNets/graphs_$n ProcessNets/graphs_orig_$n
 	cp $n/[0-9]*.csv ProcessNets/graphs_$n
 	cp "$n/edge count.tsv" ProcessNets/$folder
 	rm -r $n
