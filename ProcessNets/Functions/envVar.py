9aefd78c75f8ef7f02b38d5b170c00b66bc6ed64
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2026-07-13 16:59:39 +0530

    Update envVar.py

--- ProcessNets/Functions/envVar.py
+++ ProcessNets/Functions/envVar.py
@@ -9,7 +9,11 @@ Created on Thu Jul 18 13:32:06 2024
 import os
 
 def setThreads(threads = 10):
-    # Set environment variables to limit number of threads to 10
+    '''
+    This function sets environment variables to limit number of threads to 10. 
+    Even though we set these values to 10, our code as well as the library functions that we use from scipy, pandas, and numpy do not use multiple parallel
+    cores by default. This ensures that our benchmark comparisions are sequential as required. 
+    '''
     os.environ["OMP_NUM_THREADS"] = str(threads)
     os.environ["MKL_NUM_THREADS"] = str(threads)
     os.environ["NUMEXPR_NUM_THREADS"] = str(threads)
